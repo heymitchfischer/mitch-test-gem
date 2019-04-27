a5cb2e075d7b597daa70e4de95ff31558c397ae1
Mitch Fischer <mitchfischer6@gmail.com>
2019-04-27 11:06:49 -0500

    First Commit

---
+++ lib/mitch/test/gem.rb
@@ -0,0 +1,9 @@
+require "mitch/test/gem/version"
+
+module Mitch
+  class Test
+    def self.hello
+      puts "Hello world!"
+    end
+  end
+end
---
+++ lib/mitch/test/gem/version.rb
@@ -0,0 +1,7 @@
+module Mitch
+  module Test
+    module Gem
+      VERSION = "0.1.0"
+    end
+  end
+end
